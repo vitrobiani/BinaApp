ff36a2689448816d1d8f9f1cb040fa9157496d1b
vitrobiani <vitrobiani@gmail.com>
2026-08-01 17:12:59 +0300

    Accidantly deleted something important, oops.

---
+++ lib/pages/family/member_detail/member_detail_model.dart
@@ -0,0 +1,11 @@
+import '/app_core/app_util.dart';
+import 'member_detail_widget.dart' show MemberDetailWidget;
+import 'package:flutter/material.dart';
+
+class MemberDetailModel extends AppModel<MemberDetailWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
